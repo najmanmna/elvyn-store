151ecbc1dcdb04ceb3451fb2a389fff8d7a12318
noorjsdivs <noor.jsdivs@gmail.com>
2025-04-16 21:59:27 +0600

    middleware updated

--- middleware.ts
+++ middleware.ts
@@ -1,31 +1,12 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-// Create route matchers for protected routes
-const isProtectedRoute = createRouteMatcher([
-  "/admin(.*)",
-  "/account(.*)",
-  "/cart(.*)",
-  "/wishlist(.*)",
-]);
-
-export default clerkMiddleware(async (auth, req) => {
-  const authObject = await auth();
-
-  if (isProtectedRoute(req) && !authObject.userId) {
-    const signInUrl = new URL(
-      `/sign-in?redirect_url=${req.nextUrl.pathname}`,
-      req.url
-    );
-    return NextResponse.redirect(signInUrl);
-  }
-
-  return NextResponse.next();
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
     "/(api|trpc)(.*)",
   ],
 };
