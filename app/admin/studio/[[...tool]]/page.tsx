802895947a5d8627967f92f77a442983e6ad838a
najmanmna <139351540+najmanmna@users.noreply.github.com>
2025-09-12 22:47:33 +0530

    sanity fix

--- app/admin/studio/[[...tool]]/page.tsx
+++ app/admin/studio/[[...tool]]/page.tsx
@@ -1,10 +1,16 @@
-import { NextStudio } from "next-sanity/studio";
+"use client";
+
+import dynamic from "next/dynamic";
 import config from "../../../../sanity.config";
 
-export const dynamic = "force-static";
-
-export { metadata, viewport } from "next-sanity/studio";
+// Dynamically import NextStudio so it runs only in browser
+const NextStudio = dynamic(
+  () => import("next-sanity/studio").then((mod) => mod.NextStudio),
+  { ssr: false }
+);
 
 export default function StudioPage() {
   return <NextStudio config={config} />;
 }
+
+export { metadata, viewport } from "next-sanity/studio";
